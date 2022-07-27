2a35d45a00063d2b12f4cf2e81687588fa7eb91e
PixelsDE <carina.schoppe2912@gmail.com>
2022-07-27 19:17:09 +0200

    commit

---
+++ src/test/java/GameReadingTesting.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright Notice for Crosswise-PP
+ * Copyright (c) at Crosswise-Jacob 2022
+ * File created on 7/27/22, 7:14 PM by Carina The Latest changes made by Carina on 7/27/22, 7:14 PM All contents of "GameReadingTesting" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * at Crosswise-Jacob. All rights reserved
+ * Any type of duplication, distribution, rental, sale, award,
+ * Public accessibility or other use
+ * requires the express written consent of Crosswise-Jacob.
+ */
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class GameReadingTesting {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void readinTest() {
+
+    }
+}
