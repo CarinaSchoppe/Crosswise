bb39906a14507a69b83354fa8ece9216ae444b1d
PixelsDE <carina.schoppe2912@gmail.com>
2022-08-01 14:59:19 +0200

    finished

--- src/main/java/de/fhwwedel/pp/CrossWise.java
+++ src/main/java/de/fhwwedel/pp/CrossWise.java
@@ -13,8 +13,8 @@ package de.fhwwedel.pp;
 import de.fhwwedel.pp.ai.AI;
 import de.fhwwedel.pp.game.Game;
 import de.fhwwedel.pp.game.PlayingField;
+import de.fhwwedel.pp.gui.FakeGUI;
 import de.fhwwedel.pp.gui.GameWindow;
-import de.fhwwedel.pp.player.Player;
 import de.fhwwedel.pp.util.special.Constants;
 
 import java.util.ArrayList;
@@ -35,14 +35,14 @@ public class CrossWise {
 
     public static void main(String... args) {
         var window = new GameWindow();
+        var fakeWindow = new FakeGUI();
         new Thread(() -> {
             try {
                 if (CrossWise.SLOW) Thread.sleep(CrossWise.DELAY);
-
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-            var player1 = new Player(0, true, "Player 1");
+            var player1 = new AI(0, true, "Player 1");
             player1.create();
             var player2 = new AI(1, true, "Player 2");
             player2.create();
@@ -50,7 +50,11 @@ public class CrossWise {
             player3.create();
             var player4 = new AI(3, false, "Player 4");
             player4.create();
-            var game = new Game(new PlayingField(Constants.GAMEGRID_ROWS), new ArrayList<>(List.of(player1, player2, player3, player4)), window);
+            Game game;
+            if (SLOW)
+                game = new Game(new PlayingField(Constants.GAMEGRID_ROWS), new ArrayList<>(List.of(player1, player2, player3, player4)), window);
+            else
+                game = new Game(new PlayingField(Constants.GAMEGRID_ROWS), new ArrayList<>(List.of(player1, player2, player3, player4)), fakeWindow);
             Game.setGame(game);
             game.setup(false);
             game.start();
