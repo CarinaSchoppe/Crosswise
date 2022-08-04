582acdb568b398af8f6be791c3b652a3e452d125
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-08-04 21:52:27 +0200

    Changes by Kloevi

--- src/main/java/de/fhwwedel/pp/gui/FXGUI.java
+++ src/main/java/de/fhwwedel/pp/gui/FXGUI.java
@@ -493,7 +493,7 @@ public class FXGUI implements GUIConnector {
             case "REPLACER" -> {
                 //must be a token on the hand, must be a SymbolToken
                 return !this.clickEventSave.isGrid() &&
-                        !this.handImagesTokens.get(this.clickEventSave.getHandPosition()).isSpecial();
+                        !this.handImagesTokens.get(this.handImages[Game.getGame().getCurrentPlayer().getPlayerID()][this.clickEventSave.getHandPosition()]).isSpecial();
             }//TODO
             default -> throw new RuntimeException("Invalid token type");
         }
