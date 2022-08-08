97fa48c30768ac62da67919331c6c2199d6f0077
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-08-08 21:54:36 +0200

    commit redo

--- src/test/java/ConfigTests.java
+++ src/test/java/ConfigTests.java
@@ -1,14 +1,18 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class ConfigTests {
+class ConfigTests {
 
 
     @Test
     void configValidTest() {
 
-        var configs = new File("configs/good/");
+        var configFolder = new File("configs/good/");
+        var files = new ArrayList<File>();
+        Collections.addAll(files, configFolder.listFiles());
 
 
     }
