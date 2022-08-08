aa21378f43ed3c4d4733154392620ae8ab3ab82f
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-08-09 00:33:08 +0200

    commit redo

--- src/test/java/TeamTests.java
+++ src/test/java/TeamTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TeamTests {
+class TeamTests {
 
     @BeforeEach
     void setup() {
