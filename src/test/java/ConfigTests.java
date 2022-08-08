9006e8c728c254da8f889dfe1ab5f393e938d430
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-08-08 21:53:42 +0200

    commit redo

---
+++ src/test/java/ConfigTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class ConfigTests {
+
+
+    @Test
+    void configValidTest() {
+
+        var configs = new File("configs/good/");
+
+
+    }
+}
